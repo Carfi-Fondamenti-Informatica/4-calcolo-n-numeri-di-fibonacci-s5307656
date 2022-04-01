10f18804e37a00e666d52dedc3ba321612e0f66f
s5307656 <100590984+s5307656@users.noreply.github.com>
2022-04-01 17:24:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 int main() {
-   int n=0, primo=0, secondo= 1, terzo=0;
+   int n=0, primo=1, secondo= 1, terzo=0;
     cin >> n;
-
     if(n>=2){
        for (int i=1; i<=n; i++) {
         terzo=primo+secondo;
