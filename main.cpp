fb557da6a3e189b1b7b26adceeb94e95fb235a7f
s5307656 <100590984+s5307656@users.noreply.github.com>
2022-04-01 17:20:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   int n=0, primo=0, secondo= 1, i=0, terzo=0;
+    cin >> n;
+
+    if(n>=2){
+        for (i=1; i<=n; i++) {
+        terzo=primo+secondo;
+        primo=secondo;
+        secondo=terzo;
+
+        cout << terzo << endl;
+        }
+    } else {
+        cout << "errore" << endl;
+    }
    return 0;
 }
